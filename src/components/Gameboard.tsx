f38b22e6ee5a4c711090ae7c8f22844fdac00c91
Walter Fritsche <wfritsche95@protonmail.com>
2023-02-28 15:21:53 -0600

    added a menu when image is clicked. not functional

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -3,6 +3,7 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { storage, firestore } from "../lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { getRelativeCoords } from "../lib/utils";
+import ClickMenu from "./ClickMenu";
 
 type GameboardPropsType = {
   gameSelected: "waldo-1" | "waldo-2";
@@ -23,24 +24,29 @@ function Gameboard({
   setLastTimeScore,
 }: GameboardPropsType) {
   const [imageSrc, setImageSrc] = useState("");
+  const [showClickMenu, setShowClickMenu] = useState(false);
+  const [lastClickCoords, setLastClickCoords] = useState({x:0, y:0});
   const [waldoCoords, setWaldoCoords] = useState<CharacterCoordsType | null>();
   const [wizardCoords, setWizardCoords] = useState<CharacterCoordsType | null>();
   
   function handleImageClick(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
     // console.log("currentTarget Height", e.currentTarget.height)
-    console.dir(e);
-    console.dir(e.target);
-    console.dir(e.currentTarget);
-    console.log("Click was on:");
+    // console.dir(e);
+    // console.dir(e.target);
+    // console.dir(e.currentTarget);
     // these coordinates are the actual coordinates from the document.
     // offset values provide the compensation for margin and padding around the image
     const x = e.pageX - e.currentTarget.offsetLeft;
     const y = e.pageY - e.currentTarget.offsetTop;
+    console.log(`Click was on screenX: ${e.screenX} screenY: ${e.screenY}`);
+    console.log("Click was on:");
     console.log({x,y});
     console.log(`The size of this image is ${e.currentTarget.height} x ${e.currentTarget.width}`)
     const xRel = x / e.currentTarget.width;
     const yRel = y / e.currentTarget.height;
     console.log(`The relative coords are X: ${xRel} and Y: ${yRel}`);
+    setLastClickCoords({x: e.screenX, y: e.screenY});
+    setShowClickMenu(true);
     
   }
   useEffect(() => {
@@ -79,9 +85,10 @@ function Gameboard({
   }, []);
 
   return (
-    <div>
+    <div className="relative">
       {!imageSrc ? <p>Loading...</p> : null}
       <img onClick={handleImageClick} src={imageSrc} alt="waldo image should be here"/>
+      <ClickMenu show={showClickMenu} coords={lastClickCoords} setShowClickMenu={setShowClickMenu}/>
     </div>
   );
 }
