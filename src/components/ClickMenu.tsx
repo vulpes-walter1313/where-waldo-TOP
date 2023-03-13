56da34720bacffef9065ab30a7baedd0cf7d9530
Walter Fritsche <wfritsche95@protonmail.com>
2023-03-13 16:45:43 -0500

    ClickMenu can now pass character choosen

--- src/components/ClickMenu.tsx
+++ src/components/ClickMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 type ClickMenuProps = {
   show: boolean;
@@ -7,23 +7,49 @@ type ClickMenuProps = {
     y: number;
   };
   setShowClickMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  handleCharacterClick: (character: "waldo" | "wizard") => void;
+};
 
-}
-
-export default function ClickMenu({show, coords, setShowClickMenu}: ClickMenuProps) {
+export default function ClickMenu({
+  show,
+  coords,
+  setShowClickMenu,
+  handleCharacterClick,
+}: ClickMenuProps) {
   if (show) {
     return (
-      <div className={`bg-gray-900 fixed z-10 p-4`} style={{top: `${coords.y -100}px`, left: `${coords.x-50}px`}} onClick={() => setShowClickMenu(false)}>
+      <div
+        className={`bg-gray-900 fixed z-10 p-4`}
+        style={{ top: `${coords.y - 100}px`, left: `${coords.x - 50}px` }}
+      >
         <div className="flex justify-between text-gray-50 w-48">
           <p className="text-gray-50">Who did you find?</p>
-          <p>X</p>
+          <p className="cursor-pointer" onClick={() => setShowClickMenu(false)}>
+            X
+          </p>
         </div>
-        <div className="text-gray-50"> 
-          <p>Waldo</p>
-          <p>Wizard</p>
+        <div className="text-gray-50">
+          <p
+            className="cursor-pointer hover:bg-gray-700"
+            onClick={() => {
+              handleCharacterClick("waldo");
+              setShowClickMenu(false);
+            }}
+          >
+            Waldo
+          </p>
+          <p
+            className="cursor-pointer hover:bg-gray-700"
+            onClick={() => {
+              handleCharacterClick("wizard");
+              setShowClickMenu(false);
+            }}
+          >
+            Wizard
+          </p>
         </div>
       </div>
-    )
+    );
   } else {
     return null;
   }
