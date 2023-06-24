352b6f39d5cbaf4358f01accc9a3fefbb699ffd3
Walter Fritsche <wfritsche95@protonmail.com>
2023-06-24 15:31:57 -0500

    ran prettier

--- src/components/RecordScore.tsx
+++ src/components/RecordScore.tsx
@@ -30,33 +30,34 @@ function RecordScore({ lastTimeScore, scoreToDeleteId }: RecordScoreProps) {
         username: username,
       });
       console.log(`Added score to ${newScoreRef.id}`);
-      
     }
     setSubmitted(true);
   }
   return (
-    <div className="mx-auto flex flex-col items-center my-8">
-      <h3 className="text-slate-100 text-xl">You're in the Top Ten!</h3>
-      { submitted ? null : 
-      <div>
-        <p className="text-slate-100 capitalize">Enter a name to submit your score!</p>
-        <div className="flex flex-col my-4 gap-4">
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="border border-neutral-900 text-slate-800 bg-slate-200 rounded-md p-2"
-          />
-          <button
-            type="button"
-            className="bg-zinc-100 text-zinc-800 py-2 px-4 rounded-sm"
-            onClick={() => recordScore(lastTimeScore, scoreToDeleteId)}
-          >
-            Submit
-          </button>
+    <div className="mx-auto my-8 flex flex-col items-center">
+      <h3 className="text-xl text-slate-100">You're in the Top Ten!</h3>
+      {submitted ? null : (
+        <div>
+          <p className="capitalize text-slate-100">
+            Enter a name to submit your score!
+          </p>
+          <div className="my-4 flex flex-col gap-4">
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="rounded-md border border-neutral-900 bg-slate-200 p-2 text-slate-800"
+            />
+            <button
+              type="button"
+              className="rounded-sm bg-zinc-100 py-2 px-4 text-zinc-800"
+              onClick={() => recordScore(lastTimeScore, scoreToDeleteId)}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
-      }
+      )}
     </div>
   );
 }
