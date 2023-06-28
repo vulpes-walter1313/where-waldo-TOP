e97af96e14543690d808fc10cbfd717cabf9b35a
Walter Fritsche <wfritsche95@protonmail.com>
2023-06-28 14:28:47 -0500

    refactored to use data from react query

--- src/components/GameStats.tsx
+++ src/components/GameStats.tsx
@@ -2,6 +2,8 @@ import { collection, doc, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { firestore } from "../lib/firebase";
 import RecordScore from "./RecordScore";
+import useGetScores from "../hooks/useGetScores";
+import { ScoresBoardDataType } from "../types/types";
 
 type GameStatsProps = {
   lastTimeScore: number;
@@ -11,46 +13,46 @@ type GameStatsProps = {
 function GameStats({ lastTimeScore, gameSelected }: GameStatsProps) {
   const [isInTopTen, setIsInTopTen] = useState(false);
   const [scoreToDeleteId, setScoreToDeleteId] = useState<string | null>(null);
-  const scoreBoardName = `top-scores-${
+  const scoreBoardName: "top-scores-easy" | "top-scores-hard" = `top-scores-${
     gameSelected === "waldo-1" ? "easy" : "hard"
   }`;
 
+  const { data, status } = useGetScores(scoreBoardName);
+
   useEffect(() => {
     async function getScores() {
-      const snapshot = await getDocs(collection(firestore, scoreBoardName));
-      const topTen: { id: string; score: number; username: string }[] = [];
-      snapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-        const { score, username } = doc.data();
-        topTen.push({ id: doc.id, score, username });
-      });
       // find out how to traverse the docs and find out if lastTimeScore
       // is in top ten.
-      console.log(topTen);
-      if (topTen.length < 10) {
-        setIsInTopTen(true);
-      } else {
-        topTen.push({
-          id: "this-score-id",
-          score: lastTimeScore,
-          username: "this-user",
-        });
-        topTen.sort((a, b) => a.score - b.score);
-        console.log("top ten with this score in there");
-        console.log(topTen);
-        if (topTen.findIndex((item) => item.id === "this-score-id") < 10) {
-          console.log("this user is in the top 10");
+      if (status === "success") {
+        console.log(data);
+        if (data.length < 10) {
           setIsInTopTen(true);
-          setScoreToDeleteId(topTen[10].id);
-          console.log(`Score to be deleted is ${topTen[10].id}`);
         } else {
-          console.log("this user is not in the top 10");
-          setIsInTopTen(false);
+          const topTen: ScoresBoardDataType[] = JSON.parse(
+            JSON.stringify(data)
+          );
+          topTen.push({
+            id: "this-score-id",
+            score: lastTimeScore,
+            username: "this-user",
+          });
+          topTen.sort((a, b) => a.score - b.score);
+          console.log("top ten with this score in there");
+          console.log(topTen);
+          if (topTen.findIndex((item) => item.id === "this-score-id") < 10) {
+            console.log("this user is in the top 10");
+            setIsInTopTen(true);
+            setScoreToDeleteId(topTen[10].id);
+            console.log(`Score to be deleted is ${topTen[10].id}`);
+          } else {
+            console.log("this user is not in the top 10");
+            setIsInTopTen(false);
+          }
         }
       }
     }
     getScores();
-  }, []);
+  }, [data, status]);
 
   return (
     <div className="mx-auto w-[35rem] bg-slate-600 p-8">
