36a37c1b1428d35007eafb72d789883b3d10fc5e
Walter Fritsche <wfritsche95@protonmail.com>
2023-06-27 21:53:38 -0500

    refactored code to use react query

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,37 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { ScoresBoardDataType } from "../types/types";
+import type { ScoresBoardDataType } from "../types/types";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "../lib/firebase";
 import { twMerge } from "tailwind-merge";
+import { useQuery } from "@tanstack/react-query";
 
 type ScoreBoardProps = {
   scoreBoard: "top-scores-easy" | "top-scores-hard";
   className?: string;
 };
 
-function ScoreBoard({scoreBoard, className}: ScoreBoardProps) {
-  const [scores, setScores] = useState<ScoresBoardDataType[] | null>(null);
-  const containerStyles = "bg-slate-700 px-8 py-4 rounded-md"
-  const styles = twMerge(containerStyles, `${containerStyles} ${className}`);
-  
-  useEffect(() => {
-    const getData = async () => {
+function ScoreBoard({ scoreBoard, className }: ScoreBoardProps) {
+  const { data, isInitialLoading, isLoading, isError, error } = useQuery({
+    queryKey: ["scores", scoreBoard],
+    queryFn: async () => {
       const snapshot = await getDocs(collection(firestore, scoreBoard));
       const topTen: ScoresBoardDataType[] = [];
       snapshot.forEach((doc) => topTen.push(doc.data() as ScoresBoardDataType));
-      setScores(topTen);
-    }
-    getData()
-  }, [])
-  if (scores === null) {
+      return topTen;
+    },
+  });
+  const containerStyles = "bg-slate-700 px-8 py-4 rounded-md";
+  const styles = twMerge(containerStyles, `${containerStyles} ${className}`);
+
+  if (isInitialLoading || isLoading) {
     return <p>Loading...</p>;
   }
+
+  if (isError) {
+    return (
+      <p>
+        Error:{" "}
+        {error instanceof Error ? error.message : "Some error in React Query"}
+      </p>
+    );
+  }
+
   return (
     <div className={styles}>
-      <p className="text-lg text-slate-50 mb-2">{scoreBoard.includes("easy") ? "Top Scores - Easy" : "Top Scores - Hard"}</p>
-      <hr className="border-slate-50 mb-2"/>
-      <ol className="list-decimal text-slate-50 list-inside">
-        {scores
+      <p className="mb-2 text-lg text-slate-50">
+        {scoreBoard.includes("easy")
+          ? "Top Scores - Easy"
+          : "Top Scores - Hard"}
+      </p>
+      <hr className="mb-2 border-slate-50" />
+      <ol className="list-inside list-decimal text-slate-50">
+        {data
           .sort((a, b) => a.score - b.score)
           .map((score) => (
             <li key={score.score}>
